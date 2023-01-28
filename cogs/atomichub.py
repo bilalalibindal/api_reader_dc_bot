d3599992ec0159ebbe6e80d99799c70194a2804e
Bilal Bindal <bilalalibindal@gmail.com>
2023-01-29 01:52:21 +0300

    Update atomichub.py

--- cogs/atomichub.py
+++ cogs/atomichub.py
@@ -9,14 +9,15 @@ import asyncio
 class alcor(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.atomichub_collection_url = "https://wax.atomichub.io/explorer/collection/wax-mainnet/"
-        self.atomichub_api_url = "https://wax.api.atomicassets.io/atomicassets/v1/collections?page=1&limit=1&order=desc&sort=created"
+        self.atomichub_collection_url = "https://wax-test.atomichub.io/explorer/collection/wax-testnet/"
+        self.atomichub_api_url = "https://test.wax.api.atomicassets.io/atomicassets/v1/collections?page=1&limit=1&order=desc&sort=created"
         self.common_img_url = "https://resizer.atomichub.io/images/v1/preview?ipfs=QmYeWdTRTEePZasagQaT4UkuRbfe1sPvyw47PZrroba9tH&size=370"
         self.collection_img_url = "https://resizer.atomichub.io/images/v1/preview?ipfs="
         self.collection_name = ""
         self.old_collection_name = "a"
         self.collection_url = ""
         self.collection_img = ""
+        self.collection_name2 = ""
 
     def check_name(self):
         url = f"{self.atomichub_api_url}"
@@ -24,6 +25,13 @@ class alcor(commands.Cog):
         data = response.text
         self.collection_name = data.split("collection_name")[1][3:].split(",")[0][:-1]
         return self.collection_name
+
+    def check_name2(self):
+        url = f"{self.atomichub_api_url}"
+        response = requests.get(url)
+        data = response.text
+        self.collection_name2 = data.split("name")[2][3:].split(",")[0][:-1]
+        return self.collection_name2
 
     def check_url(self):
         try:
@@ -45,9 +53,10 @@ class alcor(commands.Cog):
     async def send(self):
         await self.bot.wait_until_ready()
         while not self.bot.is_closed():
+
+            self.collection_name = self.check_name()
             if self.collection_name != self.old_collection_name:
                 self.collection_img = self.check_img()
-                self.collection_name = self.check_name()
                 self.collection_url = self.check_url()
                 guild = self.bot.get_guild(1068249104406290572)
                 channel = discord.utils.get(guild.channels, name="atomichub")
@@ -72,11 +81,16 @@ class alcor(commands.Cog):
 
                 await channel.send(embed=embed)
                 self.old_collection_name = self.collection_name
-            await asyncio.sleep(60)  # sleep for 5 minutes
+            await asyncio.sleep(3)  # sleep for 5 minutes
 
     @commands.Cog.listener()
     async def on_ready(self):
         self.bot.loop.create_task(self.send())
+
+
+async def setup(bot):
+    # Add cog to bot.
+    await bot.add_cog(alcor(bot))
 
 
 async def setup(bot):
