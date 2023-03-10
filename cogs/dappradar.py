2fcf2adb76f0e6bcd3eb543f4e032fa52ac4e658
Bilal Bindal <bilalalibindal@gmail.com>
2023-03-10 13:19:25 +0300

    ed

--- cogs/dappradar.py
+++ cogs/dappradar.py
@@ -7,6 +7,7 @@ import asyncio
 
 class dappradar(commands.Cog):
     def __init__(self, bot, code):
+        self.page_result = 1384
         self.data = self.read_file("new_dapps_id.json")
         self.bot = bot
         self.code = code
